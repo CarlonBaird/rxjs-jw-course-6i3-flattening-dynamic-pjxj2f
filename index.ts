63b60af4cdc8994208c9c4eab897a61af0e8703d
CarlonBaird <carlyestrella@live.com>
2022-11-24 16:24:00 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 const endpointInput: HTMLInputElement =
   document.querySelector('input#endpoint');
 const fetchButton = document.querySelector('button#fetch');
 
-fromEvent(fetchButton, 'input')
-  .pipe()
+fromEvent(fetchButton, 'click')
+  .pipe(map(() => endpointInput.value))
   .subscribe((value) => console.log(value));
